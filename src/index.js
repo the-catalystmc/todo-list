459330c0eb85e62984d948ba994f04f8e1d3092f
Rhyine <rhyinestewart@gmail.com>
2021-07-13 23:07:15 -0500

    Outline the html and css for app

--- src/index.js
+++ src/index.js
@@ -1,15 +1,15 @@
-import _ from 'lodash';
+import _, { before } from 'lodash';
 import './style.css';
 
-function component() {
-    const element = document.createElement('div');
-  
-    // Lodash, currently included via a script, is required for this line to work
-    // Lodash, now imported by this script
-    element.innerHTML = _.join(['Hello', 'webpack'], ' ');
-    element.classList.add('hello');
-  
-    return element;
-  }
-  
-  document.body.appendChild(component());
+const taskData = [
+  {
+    description: 'wash the dishes',
+    completed: false,
+    index: 0,
+  },
+  {
+    description: 'write javascript code',
+    completed: false,
+    index: 1,
+  },
+]
